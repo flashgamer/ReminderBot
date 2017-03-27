c97c1d94e1d23123d26b816f2c1a8ada96c3ff79
Jon Jiang <jon.jiang1@gmail.com>
2017-03-27 01:54:03 -0400

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Main code body for Twitter Reminder Bot
+
+Twitter Reminder Bot is a TwitterBot that takes in an event and a time and
+tweets at the user at the given time reminding them of the event."""
+
+#imports
+
+__author__ = "Jonathan Jiang"
+__credits__ = ["Jonathan Jiang"]
+__version__ = "1.0.0"
+__maintainer__ = "Jonathan Jiang"
+__email__ = "jjiang85@gatech.edu"
+__status__ = "Development"
